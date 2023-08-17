304f3318495c13cd2958a141d3683c59ae85d0e4
Joey Hills <joeymhills@gmail.com>
2023-08-17 03:08:55 -0400

    dedr

--- main.go
+++ main.go
@@ -1,39 +1,24 @@
 package main
 
 import (
-	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
-type album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
-}
-
-// albums slice to seed record album data.
-var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
-}
-
 func main() {
 	gin.SetMode(gin.ReleaseMode)
 	router := gin.Default()
-	router.GET("/albums", getAlbums)
-	router.POST("/albums", postAlbums)
 
+	router.GET("/albums", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
 	var port = envPortOr("3000")
 
 	router.Run("0.0.0.0" + port)
 
-}
-func getAlbums(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, albums)
 }
 
 func envPortOr(port string) string {
@@ -43,18 +28,4 @@ func envPortOr(port string) string {
 	}
 	// Otherwise, return the value of `port` variable from function argument
 	return ":" + port
-}
-
-func postAlbums(c *gin.Context) {
-	var newAlbum album
-
-	// Call BindJSON to bind the received JSON to
-	// newAlbum.
-	if err := c.BindJSON(&newAlbum); err != nil {
-		return
-	}
-
-	// Add the new album to the slice.
-	albums = append(albums, newAlbum)
-	c.IndentedJSON(http.StatusCreated, newAlbum)
 }
