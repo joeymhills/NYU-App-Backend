81126633994471730548947ed59004d9605ef98a
Joey Hills <joeymhills@gmail.com>
2023-08-29 13:28:31 -0400

    k

--- main.go
+++ main.go
@@ -70,6 +70,8 @@ func getUsers(db *sql.DB) func(w http.ResponseWriter, r *http.Request) {
 			}
 			users = append(users, person)
 		}
+
+		w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Accept")
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(users)
