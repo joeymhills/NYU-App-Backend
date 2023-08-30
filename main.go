12d97201a525fe523cf37f77f9b5a0de3c2a7e30
Joey Hills <joeymhills@gmail.com>
2023-08-30 02:04:35 -0400

    xs

--- main.go
+++ main.go
@@ -73,7 +73,8 @@ func searchAwards(db *sql.DB) func(w http.ResponseWriter, r *http.Request) {
 		//sql query where name like %s%
 
 		awards := []Award{}
-		results, err := db.Query("SELECT id, name, institution, outcome, serviceLine, extSource, intSource, messaging, comments, frequency, notifDate, cmcontact, sourceatr, wherepubint, promotionlim, supported, createdAt FROM accolade WHERE name LIKE '%?%'", s)
+		query := "SELECT id, name, institution, outcome, serviceLine, extSource, intSource, messaging, comments, frequency, notifDate, cmcontact, sourceatr, wherepubint, promotionlim, supported, createdAt FROM accolade WHERE name LIKE %?"
+		results, err := db.Query(query, s+".%")
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			panic(err.Error())
