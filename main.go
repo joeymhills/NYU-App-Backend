9204bf926d0e2c903d063f545b40476ea504d877
Joey Hills <joeymhills@gmail.com>
2023-08-30 02:21:08 -0400

    last

--- main.go
+++ main.go
@@ -74,7 +74,7 @@ func searchAwards(db *sql.DB) func(w http.ResponseWriter, r *http.Request) {
 
 		awards := []Award{}
 		query := "%" + s + "%"
-		results, err := db.Query("SELECT id, name, institution, outcome, serviceLine, extSource, intSource, messaging, comments, frequency, notifDate, cmcontact, sourceatr, wherepubint, promotionlim, supported, createdAt FROM accolade WHERE name LIKE ", query)
+		results, err := db.Query("SELECT id, name, institution, outcome, serviceLine, extSource, intSource, messaging, comments, frequency, notifDate, cmcontact, sourceatr, wherepubint, promotionlim, supported, createdAt FROM accolade WHERE name LIKE ?", query)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			panic(err.Error())
