0d1f015102642e333f98f7571afd007be8c72aa2
Joey Hills <joeymhills@gmail.com>
2023-09-13 23:49:26 -0400

    commit slumit

--- main.go
+++ main.go
@@ -127,9 +127,9 @@ func findAward(db *sql.DB) func(w http.ResponseWriter, r *http.Request) {
         if len(s) > 0 && s[len(s)-1] == '"' {
             s = s[:len(s)-1]
         }
-        
         log.Println(s)
 
+		awards := []Award{}
         results, err := db.Query("SELECT id, name, institution, outcome, serviceLine, extSource, intSource, messaging, comments, frequency, notifDate, cmcontact, sourceatr, wherepubint, promotionlim, IFNULL(expirationDate,''), IFNULL(effectiveDate,''), IFNULL(imgurl1,''),IFNULL(imgurl2,''),IFNULL(imgurl3,''), IFNULL(imgurl4,''), supported, createdAt FROM accolade WHERE id = ?", s)
         if err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
