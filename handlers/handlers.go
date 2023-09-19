1ea4088d256e27da2b0fc7c7d03b7cdaa6d71257
Joey Hills <joeymhills@gmail.com>
2023-09-19 03:44:52 -0400

    its bed time

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -282,7 +282,7 @@ func Auth(db *sql.DB, c *cache.Cache) func(w http.ResponseWriter, r *http.Reques
         }
 
         user := User{}
-        row := db.QueryRow("SELECT * FROM user WHERE email = ?", email)
+        row := db.QueryRow("SELECT id, password, name, email, role FROM user WHERE email = ?", email)
         
         switch err := row.Scan(&user.Id, &user.Password, &user.Name, &user.Email, &user.Role);
         err {
